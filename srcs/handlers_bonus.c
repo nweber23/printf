f1821460fb6018ca3d44df6cbec849f801a5450f
Niklas Weber <nweber@student.42Heilbronn.de>
2026-09-14 12:00:59 +0200

    feat(bonus): add per-conversion handlers

---
+++ srcs/handlers_bonus.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handlers_bonus.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/14 11:59:02 by nweber            #+#    #+#             */
+/*   Updated: 2026/09/14 11:59:03 by nweber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf_bonus.h"
+
+int	handle_int(t_format *f, va_list args)
+{
+	long	value;
+	long	abs_value;
+	char	*digits;
+	char	*prefix;
+	int		ret;
+
+	value = va_arg(args, int);
+	abs_value = value;
+	if (abs_value < 0)
+		abs_value = -abs_value;
+	digits = pad_precision(get_digits(abs_value, 10, 0), f);
+	prefix = get_sign_prefix(f, value < 0);
+	ret = output_padded(f, prefix, digits, 1);
+	free(digits);
+	free(prefix);
+	return (ret);
+}
+
+int	handle_uint(t_format *f, va_list args, int base, int upper)
+{
+	unsigned long	value;
+	char			*digits;
+	char			*prefix;
+	int				ret;
+
+	value = va_arg(args, unsigned int);
+	digits = pad_precision(get_digits(value, base, upper), f);
+	prefix = get_hash_prefix(f, value);
+	ret = output_padded(f, prefix, digits, 1);
+	free(digits);
+	free(prefix);
+	return (ret);
+}
+
+int	handle_str(t_format *f, va_list args)
+{
+	char	*s;
+	char	*digits;
+	int		ret;
+
+	s = va_arg(args, char *);
+	if (!s)
+		s = "(null)";
+	if (f->has_precision)
+		digits = ft_substr(s, 0, f->precision);
+	else
+		digits = ft_strdup(s);
+	ret = output_padded(f, "", digits, 0);
+	free(digits);
+	return (ret);
+}
+
+int	handle_char(t_format *f, va_list args)
+{
+	char	digits[2];
+	int		ret;
+
+	digits[0] = (char)va_arg(args, int);
+	digits[1] = '\0';
+	ret = output_padded(f, "", digits, 0);
+	return (ret);
+}
+
+int	handle_ptr(t_format *f, va_list args)
+{
+	void	*p;
+	char	*digits;
+	int		ret;
+
+	p = va_arg(args, void *);
+	if (!p)
+		return (output_padded(f, "", NULL_PTR, 0));
+	digits = pad_precision(get_digits((unsigned long)p, 16, 0), f);
+	ret = output_padded(f, "0x", digits, 1);
+	free(digits);
+	return (ret);
+}
